002583a3918e499f1ad8ff0df2cbd799defb28de
Aaron Adrian <aaroncadrian@gmail.com>
2023-12-20 14:35:14 -0600

    Add game summary page

--- apps/web-phase-score/src/app/app-routes.tsx
+++ apps/web-phase-score/src/app/app-routes.tsx
@@ -1,9 +1,14 @@
 import { RouteObject } from 'react-router-dom';
 import { App } from './app';
+import { GameSummaryPage } from './features/feature-game-summary/game-summary-page';
 
 export const APP_ROUTES: RouteObject[] = [
   {
     index: true,
     element: <App />,
+  },
+  {
+    path: 'game-summary',
+    element: <GameSummaryPage />,
   },
 ];
---
+++ apps/web-phase-score/src/app/features/feature-game-summary/game-summary-page.tsx
@@ -0,0 +1,7 @@
+export const GameSummaryPage = () => {
+  return (
+    <div>
+      <h1>Game Summary Page</h1>
+    </div>
+  );
+};
